183046e6b8fdc4c46f5daf0beb65cbffa5aa47f9
Marko Mijajlovic <marko.b.mijajlovic@gmail.com>
2023-11-23 13:04:43 +0100

    got all controllers and controller packages

--- src/main/kotlin/com/github/mijamarko/apiforge/services/ControllerService.kt
+++ src/main/kotlin/com/github/mijamarko/apiforge/services/ControllerService.kt
@@ -5,30 +5,53 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.PsiManager
+import org.jetbrains.uast.util.isInstanceOf
+import java.util.stream.Collectors
+import kotlin.reflect.jvm.internal.impl.load.java.structure.JavaPackage
+import kotlin.streams.toList
 
 @Service(Service.Level.PROJECT)
 class ControllerService(project: Project) {
 
     private val psiCollection = ArrayList<PsiFile>()
+    private val controllerPackages = ArrayList<String?>()
 
-    fun scanForControllers(project: Project) {
+    fun loadService(project: Project) {
+        psiCollection.addAll(scanForControllers(project))
+        controllerPackages.addAll(extractControllerPackages(psiCollection))
+    }
+
+    fun scanForControllers(project: Project): Collection<PsiFile> {
         val psiManager = PsiManager.getInstance(project)
         val reg = Regex("^(@RestController|@Controller)($|\\(\\w+\\))", RegexOption.MULTILINE)
-        ProjectFileIndex.getInstance(project).iterateContent { fileOrDir ->
-            ReadAction.run<Exception> {
+        return ReadAction.compute<List<PsiFile>, Exception> {
+            val controllers = ArrayList<PsiFile>()
+            ProjectFileIndex.getInstance(project).iterateContent { fileOrDir ->
                 if (!fileOrDir.isDirectory && fileOrDir.extension == "java") {
                     psiManager.findFile(fileOrDir)?.let {
                         val contents = it.viewProvider.contents
                         if (reg.containsMatchIn(contents)){
-                            psiCollection.add(it)
+                            controllers.add(it)
                         }
                     }
                 }
+                true
             }
-            true
+            controllers
         }
     }
 
+    fun extractControllerPackages(psiCollection: Collection<PsiFile>): List<String?> {
+        return psiCollection.stream()
+            .map {
+                if (it is PsiJavaFile) {
+                    it.packageName
+                } else {
+                    null
+                }
+            }.filter{ it != null }.toList().distinct()
+    }
 
 }
